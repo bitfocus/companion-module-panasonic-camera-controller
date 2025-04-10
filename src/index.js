a5fcc37297daf5500308387cd9b84c6657ac51ac
premultiply <4681172+premultiply@users.noreply.github.com>
2025-04-10 16:47:23 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -64,8 +64,8 @@ class PTZControllerInstance extends InstanceBase {
 
 	enablePolling() {
 		clearInterval(this.pollID)
-		this.pollID = setInterval(() => this.pullData(), this.config.pollingrate)
-		this.log('debug', 'Polling enabled with ' + this.config.pollingrate + 'ms interval')
+		this.pollID = setInterval(() => this.pullData(), this.config.apiPollInterval)
+		this.log('debug', 'Polling enabled with ' + this.config.apiPollInterval + 'ms interval')
 	}
 
 	disablePolling() {
@@ -100,9 +100,11 @@ class PTZControllerInstance extends InstanceBase {
 		const cmds = [`XQC:01`, `XQC:02`]
 
 		const c = new AbortController()
-		const t = AbortSignal.timeout(this.config.pollingrate - 100)
+		const t = AbortSignal.timeout(this.config.apiPollInterval - 100)
 
-		const options = { signal: AbortSignal.any([t, c.signal, this.controller.signal]) }
+		const options = { method: 'GET',
+		signal: AbortSignal.any([t, c.signal, this.controller.signal])
+	}
 
 		const requests = cmds.map((cmd) => this.getAPI(cmd, options))
 
@@ -124,10 +126,14 @@ class PTZControllerInstance extends InstanceBase {
 	}
 
 	async getAPI(cmd, options) {
-		const url = `http://${this.config.host}:${this.config.port}/cgi-bin/aw_cam?cmd=${cmd}&res=1`
+		const url = `http://${this.config.host}:${this.config.httpPort}/cgi-bin/aw_cam?cmd=${cmd}&res=1`
 		this.log('debug', 'GET ' + url)
 
-		const response = await fetch(url, options)
+		//options.mode = 'no-cors'
+
+		const myRequest = new Request(url, options);
+
+		const response = await fetch(myRequest)
 		if (!response.ok || !response.status == 200) throw new Error(`HTTP error: ${response.status}`)
 
 		this.updateStatus(InstanceStatus.Ok)
@@ -137,7 +143,8 @@ class PTZControllerInstance extends InstanceBase {
 	}
 
 	parseData(body) {
-		const lines = body.trim().split('\r\n')
+		body = body.trim()
+		const lines = body.split('\r\n')
 		const line = lines[0].trim()
 		const response = line.split(':')
 		this.log('debug', 'Response: ' + line)
